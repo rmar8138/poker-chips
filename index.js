cf890487c11d34c47f61026d8452921816ac81d9
rmar8138 <ragan.martinez@live.com>
2019-12-11 16:21:57 +1100

    Setup basic server and views directory

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+
+const app = express();
+const port = process.env.PORT || 4321;
+
+// Database
+
+mongoose.connect("mongodb://localhost/poker-chips", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Set promise library
+
+mongoose.Promise = global.Promise;
+mongoose.connection.on("error", err => console.log(err));
+
+// Handlebars
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Allow PUT and DELETE methods
+
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
+
+// Bodyparser
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+
+app.get("/", (req, res) => res.render("home"));
+
+// Start server
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
